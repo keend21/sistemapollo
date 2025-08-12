c50ae70a7721ef4b0a03d2064f91f3c8acd4ef51
keend21 <keend1415@gmail.com>
2025-08-12 18:33:25 -0500

    Actualización del sitio web Flutter

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "5a9d6a68970ddab9a4c9ea67997b7337",
+const RESOURCES = {"flutter_bootstrap.js": "93384eeb72b170fe61ab08a95d7c4b3c",
 "version.json": "eeff56187494050990013e6e3906f1a0",
 "index.html": "d00b7124f871b1a170128514d49aa206",
 "/": "d00b7124f871b1a170128514d49aa206",
-"main.dart.js": "acc95786d08b8affd4954b9302a6bce0",
+"main.dart.js": "37edeb274d3fe4915af98a4187dc9ab9",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
